df3ee7c558823a424f72a7ea679ef0460856c0c5
John Alvarez <johncarlosalvarez1@gmail.com>
2021-05-08 17:32:45 -0400

    add base files

---
+++ public/data.js
@@ -0,0 +1,13 @@
+data = [
+{date: 2010, value: 8190209},
+{date: 2011, value: 8272948},	
+{date: 2012, value: 8346693},		
+{date: 2012, value: 8396091},
+{date: 2013, value: 8433806},	
+{date: 2015, value: 8463049},	
+{date: 2016, value: 8469153},	
+{date: 2017, value: 8437478},	
+{date: 2018, value: 8390081},	
+{date: 2019, value: 8336817},
+
+];
